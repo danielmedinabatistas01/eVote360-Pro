1620413fbbcde587ce31bc6fa1d5f13681f193e7
its daniel <danielmedinabatistas@icloud.com>
2026-06-04 15:51:24 -0400

    Add project files.

---
+++ eVote360 Pro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eVote360_Pro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
