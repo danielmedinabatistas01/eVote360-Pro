b0daa9b184ad4b4cef9f6f1239f46f600ba315bb
pmatosbeltre-lgtm <pmatosbeltre@gmail.com>
2026-06-08 19:30:51 -0400

    Clases vacias; modulos Perla; Domain y Persistence

---
+++ eVote360Pro.Infrastructure.Persistence/Repositories/AsignacionDirigenteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360Pro.Infrastructure.Persistence.Repositories
+{
+    public class AsignacionDirigenteRepository
+    {
+    }
+}
