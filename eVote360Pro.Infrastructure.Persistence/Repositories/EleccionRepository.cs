0ad6afe1855231520951058d6a0615e5ca5ea16f
franklin jose baez tineo <franklinjosebaex@gmail.com>
2026-06-11 13:48:40 -0400

    Resolver conflicto manteniendo cambios locales

--- eVote360Pro.Infrastructure.Persistence/Repositories/EleccionRepository.cs
+++ eVote360Pro.Infrastructure.Persistence/Repositories/EleccionRepository.cs
@@ -1,10 +1,8 @@
 ﻿namespace eVote360Pro.Infrastructure.Persistence.Repositories
 {
-<<<<<<< HEAD
-    public class UsuarioRepository
-=======
+
    public class EleccionRepository
->>>>>>> eb1b559ac20d4f3cefdcf290b1a50940d224b9b1
+
     {
     }
 }
--- eVote360Pro.Infrastructure.Persistence/Repositories/UsuarioRepository.cs
+++ eVote360Pro.Infrastructure.Persistence/Repositories/UsuarioRepository.cs
@@ -1,13 +1,8 @@
 ﻿
 namespace eVote360Pro.Infrastructure.Persistence.Repositories
 {
-<<<<<<< HEAD
-   public class EleccionRepository
-    {
-
-=======
     public class UsuarioRepository
     {
->>>>>>> eb1b559ac20d4f3cefdcf290b1a50940d224b9b1
+
     }
 }
