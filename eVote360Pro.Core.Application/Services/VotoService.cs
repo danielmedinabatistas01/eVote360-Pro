1fa85f1ce7744d3fb868791dc0e9069c62fc9e35
its daniel <danielmedinabatistas@icloud.com>
2026-06-14 22:20:58 -0400

    #####

--- eVote360Pro.Core.Application/Services/VotoService.cs
+++ eVote360Pro.Core.Application/Services/VotoService.cs
@@ -1,156 +1,74 @@
-﻿using eVote360Pro.Core.Application.Dtos;
-using eVote360Pro.Core.Application.DTOs;
+﻿using AutoMapper;
+using eVote360Pro.Core.Application.Dtos;
 using eVote360Pro.Core.Application.Interfaces;
-using eVote360Pro.Core.Domain.Interfaces;
 using eVote360Pro.Core.Domain.Entities;
-using eVote360Pro.Infrastructure.Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using eVote360Pro.Core.Domain.Interfaces;
+using Org.BouncyCastle.Crypto;
 
 namespace eVote360Pro.Core.Application.Services
 {
-    public class VotoService : IVotoService
+    public class VotoService: GenericService<VotoDto, Voto>, IVotoService
     {
         private readonly IVotoRepository _repository;
-        private readonly IVotoRepository _votoRepository;
+        private readonly IMapper _mapper;
 
         public VotoService(
-            IVotoRepository repository)
-        public VotoService(IVotoRepository votoRepository)
+            IVotoRepository repository,
+            IMapper mapper)
+            : base(repository, mapper)
         {
             _repository = repository;
-            _votoRepository = votoRepository;
+            _mapper = mapper;
         }
 
-        public async Task<List<VotoDto>> GetAllAsync()
-        public async Task CrearVotoAsync(VotoDTO dto)
-        {
-            var votos = await _repository.GetAllList();
-
-            return votos.Select(x => new VotoDto
-            var entity = new Voto
-            {
-                Id = x.Id,
-                CiudadanoId = x.CiudadanoId,
-                EleccionId = x.EleccionId,
-                FechaVotacion = x.FechaVotacion
-
-            }).ToList();
-        }
-
-        public async Task<VotoDto?> GetByIdAsync(int id)
-                EleccionId = dto.EleccionId,
-                CiudadanoId = dto.CiudadanoId,
-                FechaVoto = DateTime.Now,
-                VotoDetalles = dto.VotoDetalles.Select(d => new VotoDetalle
-        {
-            var voto = await _repository.GetById(id);
-
-            if (voto == null)
-                return null;
-
-            return new VotoDto
-            {
-                Id = voto.Id,
-                CiudadanoId = voto.CiudadanoId,
-                EleccionId = voto.EleccionId,
-                FechaVotacion = voto.FechaVotacion
-                    PuestoElectivoId = d.PuestoElectivoId,
-                    CandidatoId = d.CandidatoId
-                }).ToList()
-            };
-        }
-
-        public async Task AddAsync(VotoDto dto)
-        {
-            await _repository.AddAsync(new Voto
-            {
-                CiudadanoId = dto.CiudadanoId,
-                EleccionId = dto.EleccionId,
-                FechaVotacion = dto.FechaVotacion
-            });
-            await _votoRepository.AddAsync(entity);
-        }
-
-        public async Task UpdateAsync(int id, VotoDto dto)
-        public async Task<bool> CiudadanoYaVotoAsync(int ciudadanoId, int eleccionId)
-        {
-            await _repository.UpdateAsync(id, new Voto
-            {
-                CiudadanoId = dto.CiudadanoId,
-                EleccionId = dto.EleccionId,
-                FechaVotacion = dto.FechaVotacion
-            });
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            await _repository.DeleteAsync(id);
-        }
-
-
-        public async Task<bool>CiudadanoYaVotoAsync(int ciudadanoId,int eleccionId)
+        public async Task<bool> CiudadanoYaVotoAsync(
+            int ciudadanoId,
+            int eleccionId)
         {
             return await _repository
                 .CiudadanoYaVotoAsync(
                     ciudadanoId,
                     eleccionId);
-            return await _votoRepository.CiudadanoYaVotoAsync(ciudadanoId, eleccionId);
         }
 
-        public async Task<bool>PuedeVotarAsync(int ciudadanoId,int eleccionId)
-        public async Task<int> CountCiudadanosVotaronAsync(int eleccionId)
+        public async Task<bool> PuedeVotarAsync(
+            int ciudadanoId,
+            int eleccionId)
         {
-            bool yaVoto =
-                await _repository
-                    .CiudadanoYaVotoAsync(
-                        ciudadanoId,
-                        eleccionId);
-
-            return !yaVoto;
-            return await _votoRepository.CountCiudadanosVotaronAsync(eleccionId);
+            return !await CiudadanoYaVotoAsync(
+                ciudadanoId,
+                eleccionId);
         }
 
-        public async Task RegistrarVotoAsync(VotoDto dto)
-        public async Task<List<VotoDTO>> GetByEleccionIdAsync(int eleccionId)
+        public async Task RegistrarVotoAsync(
+            VotoDto dto)
         {
             bool yaVoto =
-                await _repository
-                    .CiudadanoYaVotoAsync(
-                        dto.CiudadanoId,
-                        dto.EleccionId);
-            var votos = await _votoRepository.GetByEleccionIdAsync(eleccionId);
+                await CiudadanoYaVotoAsync(
+                    dto.CiudadanoId,
+                    dto.EleccionId);
 
             if (yaVoto)
-            return votos.Select(x => new VotoDTO
             {
                 throw new Exception(
-                    "El ciudadano ya votó.");
+                    "El ciudadano ya votó en esta elección.");
             }
 
-            await _repository.AddAsync(
-                new Voto
-                Id = x.Id,
-                EleccionId = x.EleccionId,
-                CiudadanoId = x.CiudadanoId,
-                FechaVoto = x.FechaVoto,
-                VotoDetalles = x.VotoDetalles.Select(d => new VotoDetalleDTO
-                {
-                    CiudadanoId = dto.CiudadanoId,
-                    EleccionId = dto.EleccionId,
-                    FechaVotacion = DateTime.Now
-                });
-                    Id = d.Id,
-                    VotoId = d.VotoId,
-                    PuestoElectivoId = d.PuestoElectivoId,
-                    CandidatoId = d.CandidatoId
-                }).ToList()
-            }).ToList();
+            var voto =
+                _mapper.Map<Voto>(dto);
+
+            voto.FechaVoto = DateTime.Now;
+
+            await _repository.AddAsync(voto);
         }
 
+        public async Task<int>
+            CountCiudadanosVotaronAsync(
+            int eleccionId)
+        {
+            return await _repository
+                .CountCiudadanosVotaronAsync(
+                    eleccionId);
+        }
     }
 }
