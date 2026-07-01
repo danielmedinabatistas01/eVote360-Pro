dfa3e694b4fdb4fd1a7dee1df18ea024b1681d00
its daniel <danielmedinabatistas@icloud.com>
2026-07-01 01:56:57 -0400

    .

---
+++ eVote360Pro.Infrastructure.Persistence/Migrations/20260701053153_AddParticipacionCiudadanos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eVote360Pro.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddParticipacionCiudadanos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParticipacionCiudadanos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CiudadanoId = table.Column<int>(nullable: false),
+                    EleccionId = table.Column<int>(nullable: false),
+                    FechaVotacion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParticipacionCiudadanos", x => x.Id);
+
+                    table.ForeignKey(
+                        name: "FK_ParticipacionCiudadanos_Ciudadanos_CiudadanoId",
+                        column: x => x.CiudadanoId,
+                        principalTable: "Ciudadanos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+
+                    table.ForeignKey(
+                        name: "FK_ParticipacionCiudadanos_Elecciones_EleccionId",
+                        column: x => x.EleccionId,
+                        principalTable: "Elecciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParticipacionCiudadanos_CiudadanoId",
+                table: "ParticipacionCiudadanos",
+                column: "CiudadanoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParticipacionCiudadanos_EleccionId",
+                table: "ParticipacionCiudadanos",
+                column: "EleccionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParticipacionCiudadanos");
+        }
+    }
+}
